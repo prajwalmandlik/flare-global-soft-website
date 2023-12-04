5a502603d0b73ff42d4205f19ea86d41647e22b9
MilestoneWebstudio <rzoting22@gmail.com>
2023-12-04 13:31:17 +0530

    Company Description & Feedback Added,

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -16,4 +16,60 @@ export const Data = [
         post: "Founder chairman / Gokul Foundation Trust",
         Message: "They have designed my school website. Very cooperative and timely completion of website. Fully satisfied. Thank you",
     },
+
+    {
+        id:3,
+        img: User,
+        name: "Project Stakeholder",
+        post: "Final Year Student / Project Collaborator",
+        Message: "Thank you for the exceptional work on our Final Year Project—Chatbot API with Python and NLP. Your team's professionalism and timely delivery surpassed our expectations. Your innovative solutions were instrumental in our project's success. Grateful for your outstanding service.",
+    },
+
+    {
+        id:4,
+        img: User,
+        name: "Project Team Member",
+        post: "Final Year Student / Team Collaborator",
+        Message: "Working with Flare Global Soft on our final year project, the Adhikar Application in MERN stack, was an exceptional experience. Their expertise and dedication not only delivered a robust solution but also enriched our understanding of modern technologies. Grateful for the invaluable support in shaping our academic milestone.",
+    },
+
+    {
+        id:5,
+        img: User,
+        name: "Brand Representative",
+        post: "Infinity Salon, Nashik",
+        Message: "Exceptional work by the Flare Global Soft in designing our salon's logo. The creativity and attention to detail truly captured the essence of our brand. We are impressed and grateful for the outstanding service provided. Thank you for elevating our visual identity!",
+    },
+
+    {
+        id:6,
+        img: User,
+        name: "Project Participant",
+        post: "Project Contributor",
+        Message: "Thank you, Flare Global Soft, for delivering an outstanding MLA Appointment System in Python Django. Your expertise and professionalism made our final year project a success!",
+    },
+
+    {
+        id:7,
+        img: User,
+        name: "Website Stakeholder",
+        post: "RanJaSha Technologies",
+        Message: "I commend Flare Global Soft for their outstanding work on our business website. Their professionalism and creative expertise have significantly elevated our digital identity, contributing to our brand's success. Thank you for a job well done!",
+    },
+
+    {
+        id:8,
+        img: User,
+        name: "Project Contributor",
+        post: "Final Year Student / Team Collaborator",
+        Message: "Thanks to Flare Global Soft for the exceptional support in crafting our College Information App. Your expertise and commitment made a significant impact on our final year project success. Grateful for the outstanding collaboration.",
+    },
+
+    {
+        id:9,
+        img: User,
+        name: "Project Team Member",
+        post: "Final Year Student / Team Collaborator",
+        Message: "Big thanks to the Flare! Their expertise in developing our Real Estate App for our final year project was exceptional. Professionalism and innovation at its best. Grateful for a successful collaboration.",
+    },
 ];
